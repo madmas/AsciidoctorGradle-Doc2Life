e20de673c76bef5ab7c37616878d4e2f61b96490
Markus Schlichting <markus.schlichting@canoo.com>
2015-04-14 12:29:21 +0200

    examples for including tested code by line reference

---
+++ src/main/java/demo/Business.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ * This class implements the business logic of the world domination product.
+ */
+class Business {
+
+    public int calculateKeyFigure(){
+        return 42;
+    }
+
+    public void showMyCode(){
+        System.out.println("Sample with System.out.prinln");
+    }
+
+}
